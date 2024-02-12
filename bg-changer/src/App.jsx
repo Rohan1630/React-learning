900ad01f2eacd631f6cc09bbbc558eb6e157b2fa
Rohan1630 <langherohan30@gmail.com>
2024-02-12 16:52:38 +0530

    try

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -6,11 +6,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <button className="button">Button 1</button>
-      <button className="button">Button 2</button>
-      <button className="button">Button 3</button>
-      <button className="button">Button 4</button>
-      <button className="button">Button 5</button>
+      <h1>Hello</h1>
     </>
   );
 }
