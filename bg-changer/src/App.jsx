695448f20556d7a97b7dac8e78d688c037959519
Rohan1630 <langherohan30@gmail.com>
2024-02-12 16:37:22 +0530

    bg-changer

---
+++ bg-changer/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+// Removed unused import statement
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <button className="button">Button 1</button>
+      <button className="button">Button 2</button>
+      <button className="button">Button 3</button>
+      <button className="button">Button 4</button>
+      <button className="button">Button 5</button>
+    </>
+  );
+}
+
+export default App
